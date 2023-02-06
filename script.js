17aa1a70fc31612683a731e4a9287ad54a4b6b1f
iddimsangi <adammsangi@gmail.com>
2023-02-06 08:51:23 +0300

    feat:enable Escape button to close modal

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const closeModal = () => {
   modal.classList.add('hidden');
   overLay.classList.add('hidden');
 };
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
 
 openButtons.forEach(btn => btn.addEventListener('click', openModal));
 closeButton.addEventListener('click', closeModal);
